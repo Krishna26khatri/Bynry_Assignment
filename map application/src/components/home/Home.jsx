e19f8e97395970972ca741f9c17b82bd20b11a88
krishna26khatri <krishnakhatri565@gmail.com>
2024-11-16 01:14:20 +0530

    Add Home page having Company logo

--- map application/src/components/home/Home.jsx
+++ map application/src/components/home/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
-            Home Page
+        <div className="flex items-center justify-center m-10 h-96">
+            <img src="https://cdn.prod.website-files.com/634fa233f87e0b7cc0ede3ce/634fa4931fa3ea2454f5102a_bynry%20logo.webp" alt="bynry-logo"></img>
         </div>
     )
 }
