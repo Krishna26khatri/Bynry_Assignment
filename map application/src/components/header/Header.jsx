5a9dce4c660de7c12da32d6e296954f969d24585
krishna26khatri <krishnakhatri565@gmail.com>
2024-11-16 00:40:35 +0530

    add Header

--- map application/src/components/header/Header.jsx
+++ map application/src/components/header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Link,NavLink} from 'react-router-dom'
 
 function Header() {
     return (
-        <div>
-            Header
+        <div className="border-b-4 h-20 flex items-center justify-center space-x-5 font-bold text-lg">
+        
+        
+            <Link to="">Home</Link>
+            <Link to="/profile">Profiles</Link>
+            <Link to="/admin">Admin</Link>
+        
         </div>
     )
 }
